08438993963a3b5cd4f8b61b489f2fc16c368314
Jesse Harrison <jharrison902@gmail.com>
2015-11-04 22:24:22 -0500

    Initial Commit

---
+++ Lux/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lux
+//
+//  Created by Jesse Harrison on 11/4/15.
+//  Copyright © 2015 Jesse Harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
